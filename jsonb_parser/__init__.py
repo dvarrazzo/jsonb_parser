4e08bcf99809bcda37bd405a671a774247436b24
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-02-02 15:07:06 +0100

    Expose the optimised function if available

--- jsonb_parser/__init__.py
+++ jsonb_parser/__init__.py
@@ -5,4 +5,17 @@ jsonb_parser -- parse the jsonb PostgreSQL binary format to Python objects.
 # Copyright (C) 2021 Daniele Varrazzo
 
 
+import logging
+
 from .jsonb import parse_jsonb
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+
+# Try to import the optimised extension
+try:
+    from . import _parser  # type: ignore[attr-defined]
+except ImportError:
+    logger.warning("c extension not available")
+else:
+    parse_jsonb = _parser.parse_jsonb  # noqa[F811]
