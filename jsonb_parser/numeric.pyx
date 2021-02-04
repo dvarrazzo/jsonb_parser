2ddca3ad64e69248c18eaa8941a912433ba41fa5
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-02-04 01:54:20 +0100

    Improve the C generated for numeric parsing

--- jsonb_parser/numeric.pyx
+++ jsonb_parser/numeric.pyx
@@ -1,52 +1,12 @@
 """
 jsonb_parser -- parser for numeric values.
+
+See the Python implementation in numeric.py for a description of the format.
 """
 
 # Copyright (C) 2021 Daniele Varrazzo
 
 from libc.stdint cimport uint16_t
-
-from typing import cast, Callable, Dict, Tuple, Union
-
-Buffer = Union[bytes, bytearray, memoryview]
-
-# From 'src/backend/utils/adt/numeric.c'
-
-# Numeric values are represented in a base-NBASE floating point format.
-# Each "digit" ranges from 0 to NBASE-1.  The type NumericDigit is signed
-# and wide enough to store a digit.  We assume that NBASE*NBASE can fit in
-# an int.  [...]
-# Values of NBASE other than 10000 are considered of historical interest only
-# and are no longer supported in any sense; no mechanism exists for the client
-# to discover the base, so every client supporting binary mode expects the
-# base-10000 format.
-
-NBASE = 10000
-
-# The Numeric type as stored on disk.
-
-# If the high bits of the first word of a NumericChoice (n_header, or
-# n_short.n_header, or n_long.n_sign_dscale) are NUMERIC_SHORT, then the
-# numeric follows the NumericShort format; if they are NUMERIC_POS or
-# NUMERIC_NEG, it follows the NumericLong format. If they are NUMERIC_SPECIAL,
-# the value is a NaN or Infinity.  We currently always store SPECIAL values
-# using just two bytes (i.e. only n_header), but previous releases used only
-# the NumericLong format, so we might find 4-byte NaNs (though not infinities)
-# on disk if a database has been migrated using pg_upgrade.  In either case,
-# the low-order bits of a special value's header are reserved and currently
-# should always be set to zero.
-
-# NOTE: by convention, values in the packed form have been stripped of
-# all leading and trailing zero digits (where a "digit" is of base NBASE).
-# In particular, if the value is zero, there will be no digits at all!
-# The weight is arbitrary in that case, but we normally set it to zero.
-
-# Interpretation of high bits.
-NUMERIC_SIGN_MASK = 0xC000
-NUMERIC_POS = 0x0000
-NUMERIC_NEG = 0x4000
-NUMERIC_SHORT = 0x8000
-NUMERIC_SPECIAL = 0xC000
 
 
 cdef object parse_numeric(unsigned char *data, Py_ssize_t length):
@@ -55,89 +15,54 @@ cdef object parse_numeric(unsigned char *data, Py_ssize_t length):
     Note: this is a parser for the on-disk format, not the send/recv
     format. As such it is machine-dependent and probably incomplete.
     """
-    head = _get16(data, length, 0)
-    hmsb = head & NUMERIC_SIGN_MASK  # head most significant bits
-    if hmsb == NUMERIC_SHORT:
+    cdef uint16_t head = _get16(data, length, 0)
+    if numeric_is_short(head):
         return _parse_short(data, length)
-    elif hmsb == NUMERIC_SPECIAL:
+    elif numeric_is_special(head):
         return _parse_special(head)
     else:
         return _parse_long(data, length)
 
+_nan = float("NaN")
+_pinf = float("Inf")
+_ninf = float("-Inf")
 
-# Definitions for special values (NaN, positive infinity, negative infinity).
-#
-# The two bits after the NUMERIC_SPECIAL bits are 00 for NaN, 01 for positive
-# infinity, 11 for negative infinity.  (This makes the sign bit match where
-# it is in a short-format value, though we make no use of that at present.)
-# We could mask off the remaining bits before testing the active bits, but
-# currently those bits must be zeroes, so masking would just add cycles.
-
-NUMERIC_EXT_SIGN_MASK = 0xF000  # high bits plus NaN/Inf flag bits
-NUMERIC_NAN = 0xC000
-NUMERIC_PINF = 0xD000
-NUMERIC_NINF = 0xF000
-NUMERIC_INF_SIGN_MASK = 0x2000
-
-
-def _parse_special(
-    head: int,
-    __specials: Dict[int, float] = {
-        NUMERIC_NAN: float("NaN"),
-        NUMERIC_PINF: float("Inf"),
-        NUMERIC_NINF: float("-Inf"),
-    },
-) -> float:
-    return __specials[head]
-
-
-# In the NumericShort format, the remaining 14 bits of the header word
-# (n_short.n_header) are allocated as follows: 1 for sign (positive or
-# negative), 6 for dynamic scale, and 7 for weight.  In practice, most
-# commonly-encountered values can be represented this way.
-
-NUMERIC_SHORT_SIGN_MASK = 0x2000
-NUMERIC_SHORT_DSCALE_MASK = 0x1F80
-NUMERIC_SHORT_DSCALE_SHIFT = 7
-NUMERIC_SHORT_DSCALE_MAX = (
-    NUMERIC_SHORT_DSCALE_MASK >> NUMERIC_SHORT_DSCALE_SHIFT
-)
-NUMERIC_SHORT_WEIGHT_SIGN_MASK = 0x0040
-NUMERIC_SHORT_WEIGHT_MASK = 0x003F
-NUMERIC_SHORT_WEIGHT_MAX = NUMERIC_SHORT_WEIGHT_MASK
-NUMERIC_SHORT_WEIGHT_MIN = -(NUMERIC_SHORT_WEIGHT_MASK + 1)
+cdef object _parse_special(head: uint16_t):
+    if numeric_is_nan(head):
+        return _nan
+    elif numeric_is_pinf(head):
+        return _pinf
+    elif numeric_is_ninf(head):
+        return _ninf
+    else:
+        raise ValueError(f"bad special numeric header: 0x{head:04x}")
 
 
 cdef object _parse_short(unsigned char *data, Py_ssize_t length):
-    head = _get16(data, length, 0)
+    cdef uint16_t head = _get16(data, length, 0)
 
     # assemble the integer mantissa
     num: Union[int, float] = 0
 
     # weight, in NBASE-digits
-    weight = head & NUMERIC_SHORT_WEIGHT_MASK
-    if head & NUMERIC_SHORT_WEIGHT_SIGN_MASK:
-        weight |= ~NUMERIC_SHORT_WEIGHT_MASK
-    for p in range(2, length, 2):
-        num = num * 10_000 + _get16(data, length, p)
+    cdef int weight = numeric_short_weight(head)
+    cdef int i
+    for i in range(2, length, 2):
+        num = num * 10000 + _get16(data, length, i)
 
-    ndigits = length // 2 - 1
-    shift = ndigits - weight - 1
+    cdef int ndigits = (length >> 1) - 1
+    cdef int shift = ndigits - weight - 1
     if shift > 0:
-        for _ in range(shift):
-            num /= 10_000
+        for i in range(shift):
+            num *= 0.0001
     elif shift < 0:
-        for _ in range(-shift):
-            num *= 10_000
+        for i in range(-shift):
+            num *= 10000
 
     if head & NUMERIC_SHORT_SIGN_MASK:
-        num = -num
-    return num
-
-
-# In the NumericLong format, the remaining 14 bits of the header word
-# (n_long.n_sign_dscale) represent the display scale; and the weight is
-# stored separately in n_weight.
+        return -num
+    else:
+        return num
 
 
 cdef object _parse_long(unsigned char *data, Py_ssize_t length):
@@ -156,3 +81,63 @@ cdef uint16_t _get16(
         return (<uint16_t *>(data + pos))[0]
 
     raise IndexError(f"can't access {pos}: buffer size is {length}")
+
+
+cdef extern from *:
+    """
+/*
+ * Interpretation of high bits.
+ */
+
+#define NUMERIC_SIGN_MASK       0xC000
+#define NUMERIC_POS             0x0000
+#define NUMERIC_NEG             0x4000
+#define NUMERIC_SHORT           0x8000
+#define NUMERIC_SPECIAL         0xC000
+
+#define NUMERIC_FLAGBITS(h)     ((h) & NUMERIC_SIGN_MASK)
+#define numeric_is_short(h)     (NUMERIC_FLAGBITS(h) == NUMERIC_SHORT)
+#define numeric_is_special(h)   (NUMERIC_FLAGBITS(h) == NUMERIC_SPECIAL)
+
+#define NUMERIC_EXT_SIGN_MASK   0xF000  /* high bits plus NaN/Inf flag bits */
+#define NUMERIC_NAN             0xC000
+#define NUMERIC_PINF            0xD000
+#define NUMERIC_NINF            0xF000
+#define NUMERIC_INF_SIGN_MASK   0x2000
+
+#define NUMERIC_EXT_FLAGBITS(h) ((h) & NUMERIC_EXT_SIGN_MASK)
+#define numeric_is_nan(h)       ((h) == NUMERIC_NAN)
+#define numeric_is_pinf(h)      ((h) == NUMERIC_PINF)
+#define numeric_is_ninf(h)      ((h) == NUMERIC_NINF)
+#define numeric_is_inf(h)       (((h) & ~NUMERIC_INF_SIGN_MASK) == NUMERIC_PINF)
+
+/*
+ * Short format definitions.
+ */
+
+#define NUMERIC_SHORT_SIGN_MASK         0x2000
+#define NUMERIC_SHORT_DSCALE_MASK       0x1F80
+#define NUMERIC_SHORT_DSCALE_SHIFT      7
+#define NUMERIC_SHORT_DSCALE_MAX        \
+        (NUMERIC_SHORT_DSCALE_MASK >> NUMERIC_SHORT_DSCALE_SHIFT)
+#define NUMERIC_SHORT_WEIGHT_SIGN_MASK  0x0040
+#define NUMERIC_SHORT_WEIGHT_MASK       0x003F
+#define NUMERIC_SHORT_WEIGHT_MAX        NUMERIC_SHORT_WEIGHT_MASK
+#define NUMERIC_SHORT_WEIGHT_MIN        (-(NUMERIC_SHORT_WEIGHT_MASK+1))
+
+#define NUMERIC_DSCALE_MASK             0x3FFF
+
+#define numeric_short_dscale(h) \
+        (((h) & NUMERIC_SHORT_DSCALE_MASK) >> NUMERIC_SHORT_DSCALE_SHIFT)
+#define numeric_short_weight(h) \
+    (((h) & NUMERIC_SHORT_WEIGHT_SIGN_MASK ? ~NUMERIC_SHORT_WEIGHT_MASK : 0) \
+     | ((h) & NUMERIC_SHORT_WEIGHT_MASK))
+    """
+    int numeric_is_short(uint16_t h)
+    int numeric_is_special(uint16_t h)
+    int numeric_is_nan(uint16_t h)
+    int numeric_is_pinf(uint16_t h)
+    int numeric_is_ninf(uint16_t h)
+    int numeric_short_dscale(uint16_t h)
+    int numeric_short_weight(uint16_t h)
+    uint16_t NUMERIC_SHORT_SIGN_MASK
